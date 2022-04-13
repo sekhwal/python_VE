d26b9309c7e1643183f1c318aeddc8dec8165a6f
Kumar <sekhwal@gmail.com>
2022-04-13 03:11:35 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,12 @@
+import numpy
+import pandas
+import scipy
+import matplotlib
+import sklearn
+
+print('numpy version: ',numpy.__version__)
+print('pandas version: ',pandas.__version__)
+print('scipy version: ',scipy.__version__)
+print('matplotlib version: ',matplotlib.__version__)
+print('sklearn version: ',sklearn.__version__)
+
